673f8f05f1d80d2edafc50d85dfdb054d287131e
Aditya Kousik <adit267.kousik@gmail.com>
2013-10-02 19:11:23 +0530

    Initial files
    First commit

---
+++ MS/src/com/example/database/Constants.java
@@ -0,0 +1,14 @@
+package com.example.database;
+import android.provider.*;
+
+
+public interface Constants extends BaseColumns{
+	public static final String TABLE_NAME="University";
+	public static final String NAME="name";
+	public static final String DEADLINE="deadline";
+	public static final String FEE="fee";
+	public static final String NO_LORS="no_lors";
+	public static final String NO_TRANSCRIPTS="no_transcripts";
+	
+	}
+
